fa1acc1f80fa7645135926a21dadc3bb4e753934
swaption2009 <swaption2009@gmail.com>
2016-06-21 11:13:43 -0700

    use chronic to parse date

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,10 +1,11 @@
 class TodoItem
+  require 'chronic'
   include Listable
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
   def format_date
